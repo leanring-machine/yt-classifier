7c5bdb41157ebc4838c49b114be54f98891ca711
Sungwoo Park <codest99@gmail.com>
2023-06-03 01:22:59 +0900

    fix: resume method add

--- data_info.py
+++ data_info.py
@@ -1,6 +1,7 @@
 import csv
 import requests
 import os
+import re
 
 import pandas as pd
 from googleapiclient.discovery import build
@@ -14,6 +15,33 @@ youtube = build('youtube', 'v3', developerKey=os.environ["GOOGLE_DEV_KEY"])
 category_info = pd.read_csv('data/video_category.csv').set_index('id')['name'].to_dict()
 
 
+def get_video_id(id_csv, resume_path = None):
+    """
+    Get Video ID list that crawl video info. id can add after resume_id by read resume_path file
+    :param id_csv: path that save id list csv
+    :param resume_path: path that save resume id. default is `None`
+    :return: List of video id
+    """
+    result = []
+    resume_id = None
+    if os.path.isfile(resume_path):
+        with open(resume_path, 'r', encoding='utf-8') as resume_file:
+            resume_id = resume_file.readline()
+            if (not resume_id) or len(resume_id) != 11:
+                resume_id = None
+
+    is_append = not resume_id
+    with open(id_csv, newline='', encoding='utf-8') as id_csv_file:
+        reader = csv.reader(id_csv_file)
+        for row in reader:
+            video_id = row[0]
+            if (not is_append) and resume_id == video_id:
+                is_append = True
+            if is_append:
+                result.append(video_id)
+    return result
+
+
 def api_youtube_info(video_id, info_writer):
     try:
         # 비디오에 대한 정보를 요청
@@ -23,11 +51,14 @@ def api_youtube_info(video_id, info_writer):
         ).execute()
 
         # 제목, 카테고리, 설명, 비디오 ID추출
+        if len(video_info['items']) == 0:
+            return None
         snippet = video_info['items'][0]['snippet']
         title = snippet['title']
-        category = category_info.get(int(snippet['categoryId']), "others")
+        category = category_info.get(int(snippet['categoryId']))
+        if not category:
+            return None
         # description = snippet['description']
-        video_id = video_info['items'][0]['id']
 
         # CSV 파일에 기록할 데이터를 작성
         data = [video_id, title, category]
@@ -36,42 +67,43 @@ def api_youtube_info(video_id, info_writer):
         info_writer.writerow(data)
         print(f'{video_id} complete')
         return category
-
-
     except HttpError as error:
         print(error.content)
         print('An HTTP error {} occurred:\n{}'.format(error.resp.status, error.content))
+        raise error
     except Exception as e:
         print('-'*10)
+        print(e)
+        print()
         print(video_info)
+        print()
         print(f'video_id : {video_id}')
         print('-' * 10)
-        print(e)
 
 
-
-def crawl_youtube_csv(file_path):
-    if not os.path.exists("thumbnail"):
-        os.makedirs("thumbnail")
-
+def crawl_youtube_csv(video_ids, resume_path):
     with open('data/video_info.csv', mode='a', encoding='utf-8', newline='') as info_file:
         info_writer = csv.writer(info_file)
-        info_writer.writerow(['video_id', 'title', 'category'])
-        with open(file_path, newline='', encoding='utf-8') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                video_id = row[0]
-
-                category = api_youtube_info(video_id, info_writer)
-
-                thumbnail_url = f'https://img.youtube.com/vi/{video_id}/mqdefault.jpg'
+        if not os.path.isfile(resume_path):
+            info_writer.writerow(['video_id', 'title', 'category'])
+        for vid in video_ids:
+            try:
+                category = api_youtube_info(vid, info_writer)
+                if not category:
+                    continue
+                thumbnail_url = f'https://img.youtube.com/vi/{vid}/mqdefault.jpg'
                 response = requests.get(thumbnail_url)
-
-                if not os.path.exists(f"thumbnail/{category}"):
-                    os.makedirs(f"thumbnail/{category}")
-                with open(f'thumbnail/{category}/{video_id}.jpg', 'wb') as f:
+                with open(f'thumbnail/{category}/{vid}.jpg', 'wb') as f:
                     f.write(response.content)
+            except HttpError:
+                with open(resume_path, mode='w', encoding='utf-8'):
+                    print(f'API exceed!! Next time it will download info start at ${vid}')
+                    csv.writer(info_file).writerow(vid)
 
-# ids = crawl_youtube_csv('data/yt_id.csv')
 
-crawl_youtube_csv('data/yt_id.csv')
+ids = get_video_id('data/yt_id.csv', 'data/resume_id')
+if not os.path.exists("thumbnail"):
+    os.makedirs("thumbnail")
+    for c in category_info.values():
+        os.makedirs(f"thumbnail/{c}")
+crawl_youtube_csv(ids, 'data/resume_id')
--- data_link.py
+++ data_link.py
@@ -1,20 +1,23 @@
 import csv
 import time
 import re
+import requests
+
 from selenium import webdriver
 from urllib.parse import quote
 
 
-def get_video_ids(query):
+def is_shorts(vid):
+    return requests.head(f'https://www.youtube.com/shorts/{vid}', allow_redirects=False).status_code == 200
+
+
+def get_video_ids_from_link(search_url):
     driver = webdriver.Chrome()
-    # 유튜브 검색 페이지 URL 생성
-    query_encoded = quote(query.encode("utf-8"))
-    search_url = f"https://www.youtube.com/results?search_query={query_encoded}"
     # 검색 페이지 열기
     driver.get(search_url)
     # 스크롤을 위한 페이지 높이 계산
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
-    for _ in range(100):
+    for _ in range(50):
         # 현재 스크롤 위치에서 다음 위치로 스크롤
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
         # 스크롤이 더 이상 되지 않을 때까지 기다림
@@ -39,6 +42,16 @@ with open("data/search_query.csv", "r") as query_file:
     reader = csv.reader(query_file)
     result_ids = set()
     for query in reader:
-        ids = get_video_ids(query[0])
+        query_encoded = quote(query[0].encode("utf-8"))
+        search_url = f"https://www.youtube.com/results?search_query={query_encoded}"
+        ids = get_video_ids_from_link(search_url)
         result_ids.update(ids)
-    save_to_csv("data/yt_id.csv", result_ids)
+    trending_ids = filter(
+        lambda id: not is_shorts(id),
+        get_video_ids_from_link('https://www.youtube.com/feed/trending')
+    )
+    result_ids.update(trending_ids)
+    for tid in trending_ids:
+        ids = get_video_ids_from_link(f'https://www.youtube.com/watch?v=${tid}')
+        result_ids.update(ids)
+    save_to_csv("data/yt_id.csv", filter(lambda id: not is_shorts(id), result_ids))
