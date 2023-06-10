4ca4988a2874f97627d4fbc687e63d15b13dd0e4
Sungwoo Park <codest99@gmail.com>
2023-06-10 23:39:27 +0900

    refactor: add docs and refactor

--- data_link.py
+++ data_link.py
@@ -12,15 +12,17 @@ def is_shorts(vid):
 
 
 def get_video_ids_from_link(search_url):
+    """
+    Open url in selenium and crawl all vids in page
+    :param search_url: link that crawl all vids
+    :return: list of vids
+    """
+
     driver = webdriver.Chrome()
-    # 검색 페이지 열기
     driver.get(search_url)
-    # 스크롤을 위한 페이지 높이 계산
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
-    for _ in range(50):
-        # 현재 스크롤 위치에서 다음 위치로 스크롤
+    for _ in range(100):
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-        # 스크롤이 더 이상 되지 않을 때까지 기다림
         time.sleep(2)
         new_height = driver.execute_script("return document.documentElement.scrollHeight")
         if new_height == last_height:
@@ -31,27 +33,38 @@ def get_video_ids_from_link(search_url):
     return set(re.findall(r"watch\?v=(\S{11})", html))
 
 
-def save_to_csv(filename, links):
+def save_to_csv(filename, ids):
+    """
+    saving video ids at csv file
+    :param filename: file path where to save
+    :param ids: id list
+    """
     with open(filename, "a", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        for link in links:
-            writer.writerow([link])
+        for vid in ids: writer.writerow([vid])
 
 
+'''
+Get Video ID from Youtube webpage
+1. Youtube main page (suggest default video in Korea now)
+2. Search Keywords about category (from https://gist.github.com/dgp/1b24bf2961521bd75d6c)
+3. Trending videos in Korea (now)
+'''
+
+result_ids = get_video_ids_from_link('https://www.youtube.com')
 with open("data/search_query.csv", "r") as query_file:
     reader = csv.reader(query_file)
-    result_ids = set()
     for query in reader:
         query_encoded = quote(query[0].encode("utf-8"))
         search_url = f"https://www.youtube.com/results?search_query={query_encoded}"
         ids = get_video_ids_from_link(search_url)
         result_ids.update(ids)
-    trending_ids = filter(
-        lambda id: not is_shorts(id),
-        get_video_ids_from_link('https://www.youtube.com/feed/trending')
-    )
-    result_ids.update(trending_ids)
-    for tid in trending_ids:
-        ids = get_video_ids_from_link(f'https://www.youtube.com/watch?v=${tid}')
-        result_ids.update(ids)
-    save_to_csv("data/yt_id.csv", filter(lambda id: not is_shorts(id), result_ids))
+trending_ids = filter(
+    lambda id: not is_shorts(id),
+    get_video_ids_from_link('https://www.youtube.com/feed/trending')
+)
+result_ids.update(trending_ids)
+for tid in trending_ids:
+    ids = get_video_ids_from_link(f'https://www.youtube.com/watch?v=${tid}')
+    result_ids.update(ids)
+save_to_csv("data/yt_id.csv", filter(lambda id: not is_shorts(id), result_ids))
